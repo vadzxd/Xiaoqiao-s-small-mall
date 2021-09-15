dba77d37069cc1c4112acb4c20b7aa2722d07aeb
zhouxudong <377796421@qq.com>
2021-09-16 01:25:02 +0800

    feat: 合并分支

--- src/main/java/com/lsqzxd/xiaoqiaossmallmall/XiaoqiaoSSmallMallApplication.java
+++ src/main/java/com/lsqzxd/xiaoqiaossmallmall/XiaoqiaoSSmallMallApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
 public class XiaoqiaoSSmallMallApplication {
-
+    // 启动方法
     public static void main(String[] args) {
         SpringApplication.run(XiaoqiaoSSmallMallApplication.class, args);
     }
