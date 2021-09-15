b0cbf0e9b561d2d534f85f737474aef7649a1a83
zhouxudong <377796421@qq.com>
2021-09-16 00:30:35 +0800

    feat: 初始化项目文件

---
+++ src/main/java/com/lsqzxd/xiaoqiaossmallmall/XiaoqiaoSSmallMallApplication.java
@@ -0,0 +1,13 @@
+package com.lsqzxd.xiaoqiaossmallmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class XiaoqiaoSSmallMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaoqiaoSSmallMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsqzxd/xiaoqiaossmallmall/XiaoqiaoSSmallMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsqzxd.xiaoqiaossmallmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoqiaoSSmallMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
